b5243ea9d6b734ffaa8a9e3e19670a2706344fa1
Enow Mbi <benowmbi@yahoo.com>
2022-09-17 00:13:35 +0100

    Add methods using TDD

--- lib/converty.rb
+++ lib/converty.rb
@@ -3,6 +3,16 @@
 require_relative "converty/version"
 
 module Converty
+  KILOMETERS_PER_MILE = 1.609344
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.convert(value, from:, to:)
+    if from == :mi && to == :km
+      value * KILOMETERS_PER_MILE
+    elsif from == :km && to == :mi
+      value / KILOMETERS_PER_MILE
+    else
+      raise "Can't Convert error"
+    end
+  end
 end
--- spec/converty_spec.rb
+++ spec/converty_spec.rb
@@ -4,4 +4,18 @@ RSpec.describe Converty do
   it "has a version number" do
     expect(Converty::VERSION).not_to be nil
   end
+
+  describe ".convert" do
+    it "supports from miles to kilometers" do
+      expect(Converty.convert(3.1, from: :mi, to: :km).round(1)).to eq(5.0)
+    end
+
+    it "supports from kilometers to miles" do
+      expect(Converty.convert(5.0, from: :km, to: :mi).round(1)).to eq(3.1)
+    end
+
+    xit "raises an error when units are not supported" do
+      expect(Converty.convert(5.0, from: :lb, to: :kg)).to raise_error
+    end
+  end
 end
